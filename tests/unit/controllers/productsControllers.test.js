16074f4d3131014277e2b95a21d00ec2e63b535a
raphabarzotto <raphabarzotto@gmail.com>
2022-07-31 18:56:37 -0300

    test: add getById to productsControllers.test - TDD

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -9,7 +9,6 @@ const mockedProduct = { id: 1, name: 'Martelo de Thor' };
 // getAll
 describe('Get All Products (controllers/productsControllers/getAll)', () => {
   describe('When there are no registered products', () => {
-// devo mockar fora? descobrir durante o desenvolvimento
     const fakeReq = {};
     const fakeRes = {};
 
@@ -40,7 +39,6 @@ describe('Get All Products (controllers/productsControllers/getAll)', () => {
   });
 
   describe('When there are registered products', () => {
-// devo mockar fora? descobrir durante o desenvolvimento, part 2
     const fakeReq = {};
     const fakeRes = {};
 
@@ -76,6 +74,86 @@ describe('Get All Products (controllers/productsControllers/getAll)', () => {
 
     it('Arrays contains things', async () => {
       await productsControllers.getAll(fakeReq, fakeRes);
+
+      const args = fakeRes.json.args[2];
+      const firstArg = args[0];
+      const product = firstArg[0];
+
+      expect(product).to.be.an('object');
+    });
+  });
+});
+
+// getById
+describe('Get By ID (controllers/productsControllers/getById)', () => {
+  describe('No to product with given ID', () => {
+    const fakeReq = {};
+    const fakeRes = {};
+
+    before(() => {
+      fakeReq.params = { id: 1 };
+      const result = { code: 200, serviceResponse: undefined };
+      fakeReq.body = {};
+      fakeRes.status = sinon.stub().returns(fakeRes);
+      fakeRes.json = sinon.stub().returns();
+
+      sinon.stub(productsServices, 'getById').resolves(result);
+    });
+
+    after(() => {
+      productsServices.getById.restore();
+    });
+
+    it('Returns status 404', async () => {
+      await productsControllers.getById(fakeReq, fakeRes);
+
+      expect(fakeRes.status.calledWith(404)).to.be.true;
+    });
+
+    it('Retuns JSON undefined', async () => {
+      await productsControllers.getById(fakeReq, fakeRes);
+
+      expect(fakeRes.json.calledWith({ message: 'Product not found' })).to.be.true;
+    });
+  });
+
+  describe('Yes to product with given ID', () => {
+    const fakeReq = {};
+    const fakeRes = {};
+
+    before(() => {
+      const response = {
+        code: 200,
+        serviceResponse: [
+          mockedProduct
+        ]
+      };
+      fakeReq.params = { id: 1 };
+      sinon.stub(productsServices, 'getById').resolves(response);
+
+      fakeReq.body = {};
+      fakeRes.status = sinon.stub().returns(fakeRes);
+      fakeRes.json = sinon.stub().returns();
+    });
+
+    after(() => {
+      productsServices.getById.restore();
+    });
+
+    it('Returns status 200', async () => {
+      await productsControllers.getById(fakeReq, fakeRes);
+
+      expect(fakeRes.status.calledWith(200)).to.be.true;
+    });
+
+    it('Returns Array', async () => {
+      await productsControllers.getById(fakeReq, fakeRes);
+
+      expect(fakeRes.json.calledWith(sinon.match.array)).to.be.true;
+    });
+
+    it('Not empty array', async () => {
+      await productsControllers.getById(fakeReq, fakeRes);
 
       const args = fakeRes.json.args[2];
       const firstArg = args[0];
