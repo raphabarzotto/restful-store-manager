ea4d8397dc2c90a87b43a445e97c88cdcd40bda5
raphabarzotto <raphabarzotto@gmail.com>
2022-07-31 18:57:52 -0300

    feat: add getByAd to productsControllers

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -6,6 +6,17 @@ const getAll = async (_req, res) => {
   return res.status(code).json(serviceResponse);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const { code, serviceResponse } = await productsServices.getById(+id);
+
+  if (!serviceResponse) return res.status(404).json({ message: 'Product not found' });
+
+  return res.status(code).json(serviceResponse);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
