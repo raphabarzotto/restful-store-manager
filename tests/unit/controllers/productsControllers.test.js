1f4256508de2397df6659277cee4813544f87dad
raphabarzotto <raphabarzotto@gmail.com>
2022-08-01 17:04:39 -0300

    test: add update to productsControllers in TDD - forgot to commit

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -164,7 +164,7 @@ describe('Get By ID (controllers/productsControllers/getById)', () => {
   });
 });
 
-// empty
+// create
 describe('Create a new product - productsControllers/create', () => {
   describe('Create new product sucessfully', () => {
     const fakeReq = {};
@@ -216,6 +216,61 @@ describe('Create a new product - productsControllers/create', () => {
       await productsControllers.create(fakeReq, fakeRes);
 
       expect(fakeRes.json.calledWith({ id: 4, name: "ProdutoX" })).to.be.true;
+    });
+  });
+});
+
+// update
+describe('Update a product - productsControllers/update', () => {
+  describe('Update Sucessfully', () => {
+    const fakeReq = {};
+    const fakeRes = {};
+
+    before(() => {
+      const response = {
+        code: 200,
+        serviceResponse: {
+          id: 1,
+          name: "Martelo do Batman"
+        }
+      };
+
+      const response2 = {
+        code: 200,
+        serviceResponse: [
+          {
+            id: 1,
+            name: 'Martelo de Thor'
+          }
+        ]
+      };
+
+      fakeReq.params = { id: 1 };
+      fakeReq.body = { name: "Martelo do Batman" };
+      fakeRes.status = sinon.stub().returns(fakeRes);
+      fakeRes.json = sinon.stub().returns();
+
+      sinon.stub(productsServices, 'update').resolves(response);
+
+      sinon.stub(productsServices, 'getAll').resolves(response2);
+    });
+
+    after(() => {
+      productsServices.update.restore();
+
+      productsServices.getAll.restore();
+    });
+
+    it('Status 200', async () => {
+      await productsControllers.update(fakeReq, fakeRes);
+
+      expect(fakeRes.status.calledWith(200)).to.be.true;
+    });
+
+    it('Returns JSON with Martelo do Batman', async () => {
+      await productsControllers.update(fakeReq, fakeRes);
+
+      expect(fakeRes.json.calledWith({ id: 1, name: 'Martelo do Batman' })).to.be.true;
     });
   });
 });
