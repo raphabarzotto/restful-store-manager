b5ae8c6edc3b5a678c8da1c392ca5671ff2e1ebb
raphabarzotto <raphabarzotto@gmail.com>
2022-07-31 22:04:39 -0300

    fix: lint

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -2,5 +2,5 @@
 module.exports = (error, _req, res, _next) => {
   // debug raiz - vai ser tirado depois
   console.log(error);
-  res.status(500).json(error.message)
+  res.status(500).json(error.message);
 }; 
