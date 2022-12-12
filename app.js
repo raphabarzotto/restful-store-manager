1a32cbffd0f772a113e6675935f723cf41d3375f
raphabarzotto <raphabarzotto@gmail.com>
2022-12-12 18:41:32 -0300

    feat: changes and adaptations to work standalone

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const middlewares = require('./middlewares');
 const app = express();
 
 app.use(bodyParser.json());
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
@@ -17,7 +16,4 @@ app.use('/sales', salesRoutes);
 
 app.all('*', middlewares.handleRouteNotFound);
 app.use(middlewares.handleError);
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
